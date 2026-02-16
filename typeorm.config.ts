aef66e245ee8f1f0a4312f8b45e7cb9d7dbc335e
kirilldev <kirill.makliuk.dev@gmail.com>
2026-02-16 08:31:33 +0200

    feat: typeorm migrations

---
+++ typeorm.config.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  entities: [__dirname + 'dist/**/*.entity.{js,ts}'],
+  migrations: [__dirname + 'dist/migrations/*.{js,ts}'],
+});
